8588f424a904b48bf9de38f47a5c040f30b43fa3
Ganesha Ampuh G <ganeshaampuh@gmail.com>
2017-07-26 11:03:24 +0700

    Auto stash before merge of "develop" and "origin/develop"

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(AdminsTableSeeder::class);
-        //$this->call(CategoriesTableSeeder::class);
-        //$this->call(CitiesTableSeeder::class);
+        // $this->call(AdminsTableSeeder::class);
+        // $this->call(CategoriesTableSeeder::class);
+        // $this->call(CitiesTableSeeder::class);
     }
 }
