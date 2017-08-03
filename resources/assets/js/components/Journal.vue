0d77834fc621aab4032ea89a582df0b8e3431ce3
Ganesha Ampuh G <ganeshaampuh@gmail.com>
2017-08-03 14:37:37 +0700

    progress journal pages

---
+++ resources/assets/js/components/Journal.vue
@@ -0,0 +1,117 @@
+<template>
+	<div>
+		<div v-masonry
+             transition-duration="0.3s"
+             item-selector=".o-grid__col"
+             column-width=".o-grid__sizer"
+             class="o-grid">
+            <div class="o-grid__sizer u-3/12@lg u-6/12@sm"></div>
+
+            <div v-masonry-tile class="o-grid__col u-3/12@lg u-6/12@sm">
+                <div class="c-card c-card__upload-journal">
+	                <a href="#">
+	                    <div class="c-card__body">
+		                	<i class="fa fa-pencil-square-o fa-5x" aria-hidden="true"></i>
+	                        <h2 class="c-card--dialog__dummy__title">
+	                            Upload Journal Kamu <br> Sekarang!
+	                        </h2>
+	                    </div>
+	                </a>
+                </div>
+            </div>
+            
+            <div v-masonry-tile class="o-grid__col u-3/12@lg u-6/12@sm">
+				<div class="c-card c-card__related-journal">
+					<a href="#">
+						<img src="../../img/journal/related_search/1.jpg" class="o-img-content">
+						<div class="c-card__related-journal__content">
+							<h2 class="o-heading">
+								7 Nasi Goreng Di Bandung yang
+								Enaknya Selalu Terbayang
+							</h2>
+						</div>
+					</a>
+				</div>	
+			</div>
+			<div v-masonry-tile class="o-grid__col u-3/12@lg u-6/12@sm">
+				<div class="c-card c-card__related-journal">
+					<a href="#">
+						<img src="../../img/journal/related_search/1.jpg" class="o-img-content">
+						<div class="c-card__related-journal__content">
+							<h2 class="o-heading">
+								7 Nasi Goreng Di Bandung yang
+								Enaknya Selalu Terbayang
+							</h2>
+						</div>
+					</a>
+				</div>	
+			</div>
+			<div v-masonry-tile class="o-grid__col u-3/12@lg u-6/12@sm">
+				<div class="c-card c-card__related-journal">
+					<a href="#">
+						<img src="../../img/journal/related_search/1.jpg" class="o-img-content">
+						<div class="c-card__related-journal__content">
+							<h2 class="o-heading">
+								7 Nasi Goreng Di Bandung yang
+								Enaknya Selalu Terbayang
+							</h2>
+						</div>
+					</a>
+				</div>	
+			</div>
+			<div v-masonry-tile class="o-grid__col u-3/12@lg u-6/12@sm">
+				<div class="c-card c-card__related-journal">
+					<a href="#">
+						<img src="../../img/journal/related_search/1.jpg" class="o-img-content">
+						<div class="c-card__related-journal__content">
+							<h2 class="o-heading">
+								7 Nasi Goreng Di Bandung yang
+								Enaknya Selalu Terbayang
+							</h2>
+						</div>
+					</a>
+				</div>	
+			</div>
+			<div v-masonry-tile class="o-grid__col u-3/12@lg u-6/12@sm">
+				<div class="c-card c-card__related-journal">
+					<a href="#">
+						<img src="../../img/journal/related_search/1.jpg" class="o-img-content">
+						<div class="c-card__related-journal__content">
+							<h2 class="o-heading">
+								7 Nasi Goreng Di Bandung yang
+								Enaknya Selalu Terbayang
+							</h2>
+						</div>
+					</a>
+				</div>	
+			</div>
+			<div v-masonry-tile class="o-grid__col u-3/12@lg u-6/12@sm">
+				<div class="c-card c-card__related-journal">
+					<a href="#">
+						<img src="../../img/journal/related_search/1.jpg" class="o-img-content">
+						<div class="c-card__related-journal__content">
+							<h2 class="o-heading">
+								7 Nasi Goreng Di Bandung yang
+								Enaknya Selalu Terbayang
+							</h2>
+						</div>
+					</a>
+				</div>	
+			</div>
+			<div v-masonry-tile class="o-grid__col u-3/12@lg u-6/12@sm">
+				<div class="c-card c-card__related-journal">
+					<a href="#">
+						<img src="../../img/journal/related_search/1.jpg" class="o-img-content">
+						<div class="c-card__related-journal__content">
+							<h2 class="o-heading">
+								7 Nasi Goreng Di Bandung yang
+								Enaknya Selalu Terbayang
+							</h2>
+						</div>
+					</a>
+				</div>	
+			</div>
+
+         </div>
+	</div>
+</template>
---
+++ resources/assets/js/pages/Journal.vue
@@ -0,0 +1,40 @@
+<template>
+	<div>
+		<div class="o-section o-section--journal">
+			<div class="o-container--fluid">
+				<h1 class="o-heading u-text-center">
+					Journal
+				</h1>
+				<div class="o-filter-journal">
+					<div class="o-filter-journal__wrapper">
+						<div class="o-filter-journal__item">
+							<a href="#">Highlight</a>
+						</div>
+						<div class="o-filter-journal__item">
+							<a href="#">Food for Fun</a>
+						</div>
+						<div class="o-filter-journal__item">
+							<a href="#">Recipes</a>
+						</div>
+						<div class="o-filter-journal__item">
+							<a href="#">Peoples</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<section class="o-section o-section--grey">
+            <div class="o-container--fluid">
+                <journal></journal>
+            </div>
+        </section>	
+	</div>
+</template>
+
+<script>
+    import Journal from '../components/Journal.vue'
+
+	export default {
+        components: { Journal }
+	}
+</script>
