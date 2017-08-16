e8555e3cfb1aad018acd387cf91345b61ac25738
Dhika Putra Ardana <dhikaardana87@gmail.com>
2017-08-16 11:05:26 +0700

    new updates

--- resources/assets/js/pages/merchant/MerchantRegistration.vue
+++ resources/assets/js/pages/merchant/MerchantRegistration.vue
@@ -2,48 +2,50 @@
     <div class="merchant-registration o-section">
         <div class="o-container">
             <step active="1"></step>
-            <h2 class="o-heading u-m-x3">
-                Informasi Basic
-            </h2>
             <div class="tab-1">
                 <div class="o-container o-container--fit">
-                    <form action="">
-                        <div class="c-form-group">
-                            <dropzone
-                                id="myVueDropzone"
-                                ref="reviewUpload"
-                                url="/api/merchant/upload"
-                                >
-                                <!-- Optional parameters if any! -->
-                                <input type="hidden" name="token" value="xxx">
-                            </dropzone>
-                        </div>
-                        <div class="c-form-group c-form-group--mg2">
-                            <input type="text" id="" placeholder="Nama Tempat" autocomplete="off" class="o-input o-input--large">
-                        </div>
-                        <places name="address">
-                        </places>
-                        <label for="" class="u-weight-bold u-mb-x1">Kontak </label>
-                        <div class="c-form-group">
-                            <input type="text" id="" placeholder="No Telp" autocomplete="off" class="o-input o-input--large">
-                        </div>
-                        <div class="c-form-group">
-                            <input type="text" id="" placeholder="Alamat Email" autocomplete="off" class="o-input o-input--large">
-                        </div>
-                        <div class="c-form-group">
-                            <input type="text" id="" placeholder="Website" autocomplete="off" class="o-input o-input--large">
-                        </div>
-                    </form>
+                    <div class="c-card c-card--merchant">
+                        <h2 class="o-heading u-m-x3">
+                            Informasi Basic
+                        </h2>
+                        <form action="">
+                            <div class="c-form-group">
+                                <dropzone
+                                    id="myVueDropzone"
+                                    ref="reviewUpload"
+                                    url="/api/merchant/upload"
+                                    >
+                                    <!-- Optional parameters if any! -->
+                                    <input type="hidden" name="token" value="xxx">
+                                </dropzone>
+                            <label for="" class="u-weight-bold u-mb-x1">Upload Logo</label>
+                            </div>
+                            <div class="c-form-group c-form-group--mg2">
+                                <input type="text" id="" placeholder="Nama Tempat" autocomplete="off" class="o-input o-input--large">
+                            </div>
+                            <places name="address">
+                            </places>
+                            <label for="" class="u-weight-bold u-mb-x1">Kontak </label>
+                            <div class="c-form-group">
+                                <input type="text" id="" placeholder="No Telp" autocomplete="off" class="o-input o-input--large">
+                            </div>
+                            <div class="c-form-group">
+                                <input type="text" id="" placeholder="Alamat Email" autocomplete="off" class="o-input o-input--large">
+                            </div>
+                            <div class="c-form-group">
+                                <input type="text" id="" placeholder="Website" autocomplete="off" class="o-input o-input--large">
+                            </div>
+                        </form>
 
-                    <div class="c-form-group u-mt-x5">
-                        <router-link to="/merchant/registration/2" class="o-button o-button--primary o-button--block o-button--large"> Next </router-link>
-                    </div>
+                        <div class="c-form-group u-mt-x5">
+                            <router-link to="/merchant/registration/2" class="o-button o-button--primary o-button--block o-button--large"> Next </router-link>
+                        </div>
 
-                    <div class="c-form-group u-mt-x5">
-                        <router-link to="/merchant/registration"> < Kembali</router-link>
+                        <div class="c-form-group u-mt-x5">
+                            <router-link to="/merchant/registration"> < Kembali</router-link>
+                        </div>
                     </div>
                 </div>
-
             </div>
 
         </div>
--- resources/assets/js/pages/merchant/MerchantRegistration2.vue
+++ resources/assets/js/pages/merchant/MerchantRegistration2.vue
@@ -2,118 +2,118 @@
     <div class="merchant-registration o-section">
         <div class="o-container">
             <step active="2"></step>
-            <h2 class="o-heading u-m-x3">
-                Karakteristik Merchant
-            </h2>
             <div class="tab-1">
 
                 <div class="o-container o-container--fit">
-                    <div class="o-grid c-form-group">
-                        <div class="o-grid__col u-6/12@lg">
-                            <select class="o-input o-input--large">
-                                <option value="">Kategori</option>
-                            </select>
-                        </div>
-                        <div class="o-grid__col u-6/12@lg">
-                            <select class="o-input o-input--large">
-                                <option value="">Keyword</option>
-                            </select>
-                        </div>
-                    </div>
+                    <div class="c-card c-card--merchant">
+                        <h2 class="o-heading u-m-x3">
+                            Karakteristik Merchant
+                        </h2>
+                        <form action="">
+                            <div class="o-grid c-form-group">
+                                <div class="o-grid__col u-6/12@lg">
+                                    <input type="text" placeholder="Kategori" class="o-input o-input--large">
+                                </div>
+                                <div class="o-grid__col u-6/12@lg">
+                                    <input type="text" placeholder="Keyword" class="o-input o-input--large">
+                                </div>
+                            </div>
 
-                    <div class="c-form-group">
-                        <label for="">Pembayaran</label>
-                            <div class="o-grid">
-                                <div class="o-grid__col u-2/12@lg">
-                                    <div class="radio">
-                                        <label>
-                                            <input type="radio" value="" name="pembayaran"> Card & Cash
-                                        </label>
+                            <div class="c-form-group">
+                                <label for="">Pembayaran</label>
+                                    <div class="o-grid">
+                                        <div class="o-grid__col u-3/12@lg">
+                                            <div class="radio">
+                                                <label>
+                                                    <input type="radio" value="" name="pembayaran"> Card & Cash
+                                                </label>
+                                            </div>
+                                        </div>
+                                        <div class="o-grid__col u-3/12@lg">
+                                            <div class="radio">
+                                                <label>
+                                                    <input type="radio" value="" name="pembayaran"> Cash Only
+                                                </label>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="o-grid__col u-2/12@lg">
-                                    <div class="radio">
-                                        <label>
-                                            <input type="radio" value="" name="pembayaran"> Cash Only
-                                        </label>
+
+                            <div class="c-form-group">
+                                <label for="">Jam Buka</label>
+                                <div class="o-grid">
+                                    <div class="o-grid__col u-2/12@lg">
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox" value="">Senin
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="o-grid__col u-2/12@lg">
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox" value="">Selasa
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="o-grid__col u-2/12@lg">
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox" value="">Rabu
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="o-grid__col u-2/12@lg">
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox" value="">Kamis
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="o-grid__col u-2/12@lg">
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox" value="">Jumat
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="o-grid__col u-2/12@lg">
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox" value="">Sabtu
+                                            </label>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+
+                            <div id="column-time">
+                                <div class="append o-grid c-form-group" v-for="(columnTime, index) in columnTimes ">
+                                    <div class="o-grid__col u-4/12@lg">
+                                        <select name="" id="" v-model="columnTime.from" class="o-input o-input--large">
+                                            <option value="">Dari</option>
+                                        </select>
+                                    </div>
+                                    <div class="o-grid__col u-4/12@lg">
+                                        <select name="" id="" class="o-input o-input--large" v-model="columnTime.to">
+                                            <option value="">Sampai</option>
+                                        </select>
+                                    </div>
+                                    <div class="o-grid__col u-4/12@lg">
+                                        <a href="#" v-if="columnTimes.length == (index+1)" @click.prevent="addField()" class="o-button o-button--primary o-button--block o-button--large"> Tambah Waktu </a>
+                                        <a href="#" v-if="columnTimes.length != 1 && columnTimes.length != (index+1) " @click.prevent="removeField()" class="o-button o-button--primary o-button--block o-button--large"> Remove </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+
+                        <div class="c-form-group">
+                            <router-link to="/merchant/registration/3" class="o-button o-button--primary o-button--block o-button--large"> Next </router-link>
                         </div>
 
-                    <div class="c-form-group">
-                        <label for="">Jam Buka</label>
-                        <div class="o-grid">
-                            <div class="o-grid__col u-2/12@lg">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" value="">Senin
-                                    </label>
-                                </div>
-                            </div>
-                            <div class="o-grid__col u-2/12@lg">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" value="">Selasa
-                                    </label>
-                                </div>
-                            </div>
-                            <div class="o-grid__col u-2/12@lg">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" value="">Rabu
-                                    </label>
-                                </div>
-                            </div>
-                            <div class="o-grid__col u-2/12@lg">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" value="">Kamis
-                                    </label>
-                                </div>
-                            </div>
-                            <div class="o-grid__col u-2/12@lg">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" value="">Jumat
-                                    </label>
-                                </div>
-                            </div>
-                            <div class="o-grid__col u-2/12@lg">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" value="">Sabtu
-                                    </label>
-                                </div>
-                            </div>
+                        <div class="c-form-group u-mt-x5">
+                            <router-link to="/merchant/registration/1"> < Kembali</router-link>
                         </div>
-                    </div>
-
-                    <div id="column-time">
-                        <div class="append o-grid c-form-group" v-for="(columnTime, index) in columnTimes ">
-                            <div class="o-grid__col u-4/12@lg">
-                                <select name="" id="" v-model="columnTime.from" class="o-input o-input--large">
-                                    <option value="">Dari</option>
-                                </select>
-                            </div>
-                            <div class="o-grid__col u-4/12@lg">
-                                <select name="" id="" class="o-input o-input--large" v-model="columnTime.to">
-                                    <option value="">Sampai</option>
-                                </select>
-                            </div>
-                            <div class="o-grid__col u-4/12@lg">
-                                <a href="#" v-if="columnTimes.length == (index+1)" @click.prevent="addField()" class="o-button o-button--primary o-button--block o-button--large"> Tambah Waktu </a>
-                                <a href="#" v-if="columnTimes.length != 1 && columnTimes.length != (index+1) " @click.prevent="removeField()" class="o-button o-button--primary o-button--block o-button--large"> Remove </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="c-form-group">
-                        <router-link to="/merchant/registration/3" class="o-button o-button--primary o-button--block o-button--large"> Next </router-link>
-                    </div>
-
-                    <div class="c-form-group u-mt-x5">
-                        <router-link to="/merchant/registration/1"> < Kembali</router-link>
                     </div>
                 </div>
             </div>
