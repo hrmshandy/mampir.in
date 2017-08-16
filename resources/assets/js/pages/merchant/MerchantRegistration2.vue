8a3488e52eab513648fba80f3fd3603468f7e312
Dhika Putra Ardana <dhikaardana87@gmail.com>
2017-08-16 13:33:05 +0700

    new updates

--- resources/assets/js/pages/merchant/MerchantRegistration2.vue
+++ resources/assets/js/pages/merchant/MerchantRegistration2.vue
@@ -39,8 +39,7 @@
                                     </div>
                                 </div>
 
-                            <div class="c-form-group">
-                                <label for="">Jam Buka</label>
+                            <!-- <div class="c-form-group">
                                 <div class="o-grid">
                                     <div class="o-grid__col u-2/12@lg">
                                         <div class="checkbox">
@@ -85,25 +84,16 @@
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-
-                            <div id="column-time">
-                                <div class="append o-grid c-form-group" v-for="(columnTime, index) in columnTimes ">
-                                    <div class="o-grid__col u-4/12@lg">
-                                        <select name="" id="" v-model="columnTime.from" class="o-input o-input--large">
-                                            <option value="">Dari</option>
-                                        </select>
-                                    </div>
-                                    <div class="o-grid__col u-4/12@lg">
-                                        <select name="" id="" class="o-input o-input--large" v-model="columnTime.to">
-                                            <option value="">Sampai</option>
-                                        </select>
-                                    </div>
-                                    <div class="o-grid__col u-4/12@lg">
-                                        <a href="#" v-if="columnTimes.length == (index+1)" @click.prevent="addField()" class="o-button o-button--primary o-button--block o-button--large"> Tambah Waktu </a>
-                                        <a href="#" v-if="columnTimes.length != 1 && columnTimes.length != (index+1) " @click.prevent="removeField()" class="o-button o-button--primary o-button--block o-button--large"> Remove </a>
-                                    </div>
-                                </div>
+                            </div> -->
+                            
+                            <div class="c-form-group">
+                                <label for="">Jam Buka</label>
+                                <textarea placeholder="Contoh: Buka Senin - Sabtu 24jam" name="jamBuka" id="jamBuka" class="o-textarea o-textarea--large o-textarea__expand" cols="30" rows="20"></textarea><!-- 
+                                <tabs>
+                                    <tab name="Senin">
+                                        <Tabscontent></Tabscontent>
+                                    </tab>
+                                </tabs> -->
                             </div>
                         </form>
 
@@ -125,31 +115,13 @@
 <script>
     import Step from '../../components/StepTab.vue'
 
+    import Vue from 'vue';
+    import {Tabs, Tab} from 'vue-tabs-component';
+
+    Vue.component('tabs', Tabs);
+    Vue.component('tab', Tab);
+
     export default {
-        components: { Step },
-        data() {
-            return {
-                columnTimes: [
-                    {
-                        from : '',
-                        to : ''
-                    }
-                ]
-            }
-        },
-
-        methods: {
-            addField() {
-                this.columnTimes.push({
-                    from : '',
-                    to : ''
-                })
-            },
-            removeField(index) {
-                this.columnTimes.splice(index, 1)
-            }
-        }
-
+        components: { Step }
     }
-
 </script>
