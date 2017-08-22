84107e4e0e03beecc205286aeaa758ae4328b7f1
Sandi Hermawan <hrm.shandy05@gmail.com>
2017-08-22 10:55:21 +0700

    implement google analytic

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -60,5 +60,15 @@
     <script src="{{ mix('js/manifest.js') }}"></script>
     <script src="{{ mix('js/vendor.js') }}"></script>
 	<script src="{{ mix('js/app.js') }}"></script>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-77192730-1', 'auto');
+        ga('send', 'pageview');
+
+    </script>
 </body>
 </html>
